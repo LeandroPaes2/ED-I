e86bdfc777c901869baec280c7fde99d8ae3bab2
LeandroPaes2 <lelepaes205@gmail.com>
2025-09-24 02:03:48 -0300

    tads

---
+++ Exercicios/PilhaM2/8/Ex8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio2.h>
+#include <stdlib.h>
+
+#include "tadPilhaM2.h"
+
+
+int main(void)
+{
+	TpPilhaM2 p;
+	
+	inicializar(p, 3);
+	
+	return 0;
+}
